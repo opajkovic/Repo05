8b145440de7132c683be6d260fddbc64f314a3f6
opajkovic <oljaolja170@gmail.com>
2022-04-01 21:29:26 +0200

    added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    var myString = "FirstLinenewlinetabSecondLinenewlineThirdLine";
+    myString = "FirstLine\n\tSecondLine\nThirdLine";
+    
+
+    return myString;
+}
+console.log(main());
+module.exports = main;
