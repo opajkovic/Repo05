ab522eb6cdd5f1d306d897b04c15b1905ce9f0a8
opajkovic <oljaolja170@gmail.com>
2022-04-01 11:13:29 +0200

    added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    var myFirstName = "Olgica";
+    var myLastName = "Pajkovic";
+    var myHomeTown = "Podgorica";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
